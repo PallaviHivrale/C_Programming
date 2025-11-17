572db2dbb651eb43fb1c040826473aab41d2d77d
Pallavi Hivrale <hivralepallavi2004@gmail.com>
2025-11-17 13:46:47 +0530

    Add files via upload

---
+++ Assignments/Assignment_14/program14_4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int CountFour(int iNo)
+{
+    int iDigit = 0, iCount = 0;
+
+    if(iNo < 0) 
+        iNo = -iNo;
+
+    if(iNo == 0) 
+    {
+        return 0;
+    }
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;   
+        if(iDigit == 4)      
+        {
+            iCount++;        
+        }
+        iNo = iNo / 10;      
+    }
+
+    return iCount;          
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;           
+
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    iRet = CountFour(iValue);  
+
+    printf("Frequency of 4 is: %d\n", iRet); 
+
+    return 0;
+}
